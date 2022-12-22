49221881c286ada60bd627ae040e6002d61981f7
mahotpal1 <mahotpalvishal87@gmail.com>
2022-12-22 21:03:56 +0530

    added

---
+++ GFG/LinkedList/SinglyLinkedList/Search.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+class Node{
+    public :
+      Node* next;
+      int data;
+    };
+
+class Insertion{
+  private :
+    Node* head;
+  public :
+    Insertion(Node* n)
+      :head{n}{
+    }
+    ~Insertion(){
+      std::cout << "Destructor Called! " << std::endl;
+    }
+    void deleteFromKthLoc(int k){
+      if(head==NULL){
+        std::cout << "Linked List is empty, No Elements to be deleted!" << std::endl;
+        return;
+      }
+      Node* temp = head;
+      if(k == 1){
+        head = head->next;
+        std::cout << temp->data << " deleted from list!" << std::endl;
+        delete temp;
+        return;
+      }
+      for(int i=1; i<k-1; i++){
+        if(temp==NULL || temp->next == NULL){
+        std::cout << "Index out of Range, no elements deleted!" << std::endl;
+        return;
+      }
+        temp = temp->next;
+      }
+      
+      Node* ne = temp->next->next;
+      delete(temp->next);
+      temp->next = ne;
+    }
+
+    void append(int new_data){
+      Node* current = new Node();
+      current->next = NULL;
+      current->data = new_data;
+      Node *last = head;
+      if(head==NULL){
+        std::cout << "Empty List, Appending as Head" << std::endl;
+        head = current;
+        return;
+      }
+      while(last->next != NULL){
+        last = last->next;
+      }
+      last->next = current;
+      return;
+    }
+    void display(){
+      Node* temp = head;
+      std::cout << "Array Elements : " << std::endl;
+      while(head != NULL){
+        std::cout << head->data << " ";
+        head=head->next;
+      }
+      std::cout << std::endl;
+      head = temp;
+    }
+
+    void search(int n){
+      if(head == NULL){
+        std::cout << "Element is not there, Linked List is Empty!!" <<std::endl;
+        return;
+      }
+      Node* temp = head;
+      int count=1;
+      while(temp != NULL){
+        if(temp->data == n){
+          std::cout << "Element found at " << count << std::endl;
+          return;
+        }
+        count++;
+        temp = temp->next;
+      }
+      std::cout << "Element Not Found!!!" << std::endl;
+    }
+};
+
+
+int main(){
+  Node* head = NULL;
+  Insertion i = Insertion(head);
+  i.append(13);
+  i.append(9);
+  i.append(6);
+  i.append(2);
+  i.append(0);
+  i.append(1);
+  i.display();
+  i.display();
+  i.append(121);
+  i.append(34);
+  i.deleteFromKthLoc(3);
+  i.display();
+  i.search(13);
+  return 0;
+}
